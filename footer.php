452b8a0a52822cacdfc5cf477dfd9db925e23917
Bronson15 <bronsonglane@me.com>
2017-10-27 18:02:21 -0400

    testing part 1

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <?php echo'
 <footer>
 	<div class="contact">
-		<a href="contact.html">Contact Us</a>
-			Email:gsupizza@gsu.com<br>
+		<a href="contact.html">Contact Us</a><br>
+			Email: <a href = "mailto:gsupizza@gsu.com">gsupizza@gsu.com</a><br>
 			Phone:(912)-555-5555<br>
-			Address:123 Chandler Rd<br>
+			Address: 123 Chandler Rd<br>
 			Statesboro, GA 30458<br><br>
 	</div>
 	<div class="copy">
