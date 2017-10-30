f4af2e17d1335f6094a8ade95270ddc92666a866
Bronson15 <bronsonglane@me.com>
2017-10-30 15:38:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,17 +22,37 @@ else
 	<main>
 		
 		<?php
+$data_source='aa17n6gzzuklrjm.ceko05wsajde.us-east-2.rds.amazonaws.com:1433';
+$user='admin';
+$password='password';
+			$conn=odbc_connect($data_source,$user,$password);
+if (!$conn){
+    if (phpversion() < '4.0'){
+      exit("Connection Failed: . $php_errormsg" );
+    }
+    else{
+      exit("Connection Failed:" . odbc_errormsg() );
+    }
+}
 
-			$dbhost = $_SERVER['aa17n6gzzuklrjm.ceko05wsajde.us-east-2.rds.amazonaws.com'];
-			$dbport = $_SERVER['1433'];
-			$dbname = $_SERVER['GSUPizzaShop'];
-			$charset = 'utf8' ;
+// This query generates a result set with one record in it.
+$sql="SELECT 1 AS test_col";
 
-			$dsn = "sqlsrv:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-			$username = $_SERVER['admin'];
-			$password = $_SERVER['password'];
+# Execute the statement.
+$rs=odbc_exec($conn,$sql);
 
-			$pdo = new PDO($dsn, $username, $password);
+// Fetch and display the result set value.
+if (!$rs){
+    exit("Error in SQL");
+}
+while (odbc_fetch_row($rs)){
+    $col1=odbc_result($rs, "test_col");
+    echo "$col1\n";
+}
+
+// Disconnect the database from the database handle.
+odbc_close($conn);
+
 
 		?>
 		<p>Welcome to GSU pizza shop<p>
