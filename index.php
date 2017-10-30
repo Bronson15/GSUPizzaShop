af47f75d3b031a5aa69960ed1941ae5e44748a78
Bronson15 <bronsonglane@me.com>
2017-10-30 18:48:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -27,17 +27,10 @@ else
 			  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 			}
 			# Here we establish the connection. Yes, that's all.
-			$pg_conn = pg_connect(pg_connection_string_from_database_url());
- 			if (!$pg_conn) {
-				die("Error in connection: " . pg_last_error());
-			}
+			$pg_conn = pg_connect(pg_connection_string_from_database_url()) or die("Error in connection: " . pg_last_error());
 			# Now let's use the connection for something silly just to prove it works:
-			$result = pg_query($pg_conn, "SELECT * FROM public.'Customer'");
- 			if (!result) {
-				die("Error in SQL: " . pg_last_error());
-			} else {
-				echo "It should be working";
-			}
+			$result = pg_query($pg_conn, "SELECT * FROM customer") or die("Error in SQL: " . pg_last_error());
+			print $result;
 			
  
 		?>
