1de5d9313c25815aa84b873e928463e281a25aca
Bronson15 <bronsonglane@me.com>
2017-10-30 18:07:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -30,14 +30,8 @@ else
 			$pg_conn = pg_connect(pg_connection_string_from_database_url());
 			# Now let's use the connection for something silly just to prove it works:
 			$result = pg_query($pg_conn, "SELECT * FROM 'public'.Customer");
-			print "<pre>\n";
-			if (!pg_num_rows($result)) {
-			  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-			} else {
-			  print "Tables in your database:\n";
-			  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-			}
-			print "\n";
+			print $result;
+			
  
 		?>
 		<p>Welcome to GSU pizza shop<p>
