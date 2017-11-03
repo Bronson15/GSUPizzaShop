e9688b34c9d17ca38157648e1401261263ee07d9
Bronson15 <bronsonglane@me.com>
2017-11-03 15:18:47 -0400

    testing

--- pizza.php
+++ pizza.php
@@ -1,10 +1,21 @@
 <?php include("header.php");?>
+
+<?php $result = pg_query($pg_conn, "SELECT price FROM pizzas WHERE productid = 7;") or die("Error in SQL: " . pg_last_error());
+								$row = pg_fetch_assoc($result);
+								$price = $row['price']; ?>
+
 <main>
 	<p>Welcome to GSU pizza shop<p>
 	<p>Check out our best deals under specials</p>
 </main>
 <div id="pizza-table">
 	<table>
+		<script>
+			function changePrice(){
+				var smallPrice = "<?php echo $price; ?>";
+				document.getElementById("price").innerHTML = smallPrice;
+			}
+		</script>
 			<tr>
 				<td><b>Cheese Pizza</b>
 				<form action="cart.php">
@@ -15,40 +26,11 @@
 					
 				<select id="size" style="width:200px">
 					<option value="small">Small</option>
-					<option value="medium">Medium <?php $result = pg_query($pg_conn, "SELECT price FROM pizzas WHERE productid = 8;") or die("Error in SQL: " . pg_last_error());
-								$row = pg_fetch_assoc($result);
-								$price = $row['price']; ?></option>
+					<option value="medium">Medium </option>
 					<option value="Large">Large</option>
 				</select>
 				
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-				<script>
-					function makeAjaxRequest(price){
-						$.ajax({
-						  type: "POST",
-						  data: { price: price },
-						  dataType: "json",
-						  url: "process_ajax.php",
-						  success: function(json) {
-							insertResults(json);
-						  }
-						});
-					}
-					function insertResults(json){
-						$("#price").val(json["price"]);
-					}
-					function clearForm(){
-						$("#price").val("");
-					}
-					$("#size").on("change", function() {
-						var selected = $(this).val();
-						if (selected == "0"){
-							clearForm();
-						} else {
-							makeAjaxRequest(selected);
-						}
-					});
-				</script>
+				
 					
 				<p>
 				<select name="Style" style="width: 200px">
