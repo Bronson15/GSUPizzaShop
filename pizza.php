b956b11b247990e3cae4857f09bdb81eca659085
Bronson15 <bronsonglane@me.com>
2017-11-03 15:42:54 -0400

    testing

--- pizza.php
+++ pizza.php
@@ -1,6 +1,16 @@
 <?php 
 	include("header.php");
-	include("pizza-data.php");
+	$result = pg_query($pg_conn, "SELECT price FROM pizzas WHERE productid = 7;") or die("Error in SQL: " . pg_last_error());
+$row = pg_fetch_assoc($result);
+$smallCheese = $row['price']; 
+
+$result = pg_query($pg_conn, "SELECT price FROM pizzas WHERE productid = 8;") or die("Error in SQL: " . pg_last_error());
+$row = pg_fetch_assoc($result);
+$medCheese = $row['price']; 
+
+$result = pg_query($pg_conn, "SELECT price FROM pizzas WHERE productid = 9;") or die("Error in SQL: " . pg_last_error());
+$row = pg_fetch_assoc($result);
+$largeCheese = $row['price']; 
 ?>
 
 <main>
