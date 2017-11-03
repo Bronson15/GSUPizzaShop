7a66751b822c86b3a19e74093b7436fd9961012f
dmenear <davidlmenear@gmail.com>
2017-11-03 15:04:42 -0400

    Remove database testing

--- index.php
+++ index.php
@@ -12,37 +12,5 @@ include("header.php");
 	</div>
 	
 	<div id="database-testing">
-		<?php
-			$result = pg_query($pg_conn, "SELECT * FROM customer") or die("Error in SQL: " . pg_last_error());
-		 	$i = 0;
-			echo '<table><tr>';
-			while ($i < pg_num_fields($result))
-			{
-				$fieldName = pg_field_name($result, $i);
-				echo '<td>' . $fieldName . '</td>';
-				$i = $i + 1;
-			}
-			echo '</tr>';
-			$i = 0;
-
-			while ($row = pg_fetch_row($result)) 
-			{
-				echo '<tr>';
-				$count = count($row);
-				$y = 0;
-				while ($y < $count)
-				{
-					$c_row = current($row);
-					echo '<td>' . $c_row . '</td>';
-					next($row);
-					$y = $y + 1;
-				}
-				echo '</tr>';
-				$i = $i + 1;
-			}
-			pg_free_result($result);
-
-			echo '</table>';
-		?>
 	</div>
 	<?php include("footer.php");?>
