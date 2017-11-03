e50464fd37c9d470570f72705e25acc69094ca79
dmenear <davidlmenear@gmail.com>
2017-11-03 15:26:36 -0400

    Free the copyright text

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php 
-
-include("header.php");
-
-?>
+<?php include("header.php"); ?>
 	
 	<div id="index-body-text">		
 		<main>
@@ -10,7 +6,5 @@ include("header.php");
 			<p>Check out our best deals under specials</p>
 		</main>
 	</div>
-	
-	<div id="database-testing">
-	</div>
-	<?php include("footer.php");?>
+
+<?php include("footer.php");?>
