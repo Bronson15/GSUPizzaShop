61cbb8feaaa2023b5558d0aeb2a6f5a60eda5246
kadenjgraham <kadenjgraham@yahoo.com>
2017-11-03 15:45:18 -0400

    testing

--- specials.php
+++ specials.php
@@ -5,18 +5,21 @@
 	<p>***CARRY OUT ONLY***</p>
 	<form action="cart.php" style="background-color:#00008B">
 	<fieldset>
+	<p>Large pizza with any topping of your choosing</p>
 	<img src="/img/pizzapizza.jpg" alt="Pizza">
     <button type="add" onclick="cart.php" style="background-color:Gold">Large Combo Pizza $8.00</button>	
 	</fieldset>
 	</form>
 	<form action="cart.php" style ="background-color:#00008B">	
     <fieldset>	
+	<p>Two large cheese pizzas for the cheese lovers</p>
 	<img src= "/img/cheese.jpg" alt="CPizza">
 	<button type="add" onclick="cart.php" style="background-color:Gold">Two Large Cheese Pizzas for $9.99</button>	
 	</fieldset>
 	</form>
 	<form action="cart.php" style="background-color:#00008B">
 	<fieldset>
+	<p>For the carnivores a large meat lovers pizza</p>
 	<img src= "/img/meat.jpg" alt="MPizza">
 	<button type="add" onclick="cart.php" style="background-color:Gold">Large Meat Lovers Pizza for $12.99</button>
 	</fieldset>
