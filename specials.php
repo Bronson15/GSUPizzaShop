c31682027725736d19604a1a33f83ea57027d9f4
kadenjgraham <kadenjgraham@yahoo.com>
2017-11-03 14:52:01 -0400

    testing
    testing

--- specials.php
+++ specials.php
@@ -3,18 +3,17 @@
 <main>
 	<p>The Best Deals in Town: ALL DAY, EVERYDAY</p>
 	<p>***CARRY OUT ONLY***</p>
-	<a href="cart.php">
-		<img src= "/img/pizzapizza.jpg" alt="Pizza">
-	</a>		
-	<p><a href="cart.php">Large Combo Pizza $8.00</a></p>		
-	<a href= "cart.php">
-		<img src= "/img/cheese.jpg" alt="CPizza">
-	</a>
-	<p><a href="cart.php">Two Large Cheese Pizzas for $9.99</a></p>	
-	<a href= "cart.php">
-		<img src= "/img/meat.jpg" alt="MPizza">
-	</a>
-	<p><a href="cart.php">Large Meat Lovers Pizza for $12.99</a></p>
+	<form action="cart.php>
+	<img src= "/img/pizzapizza.jpg" alt="Pizza">
+    <button type="add" onclick="cart.php">Large Combo Pizza $8.00</button>	
+	</form>
+	<form action="cart.php">		
+	<img src= "/img/cheese.jpg" alt="CPizza">
+	<button type="add" onclick="cart.php">Two Large Cheese Pizzas for $9.99</button>	
+	</form>
+	<form action="cart.php">
+	<img src= "/img/meat.jpg" alt="MPizza">
+	<button type="add" onclick="cart.php">Large Meat Lovers Pizza for $12.99</button>
 </main>
 </div>
 <?php include("footer.php");?>
