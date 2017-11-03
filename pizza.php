c90a8156f88ffc7906d300c7eeebdfaf4291a0e3
Bronson15 <bronsonglane@me.com>
2017-11-03 14:55:21 -0400

    testing

--- pizza.php
+++ pizza.php
@@ -23,7 +23,7 @@
 				
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 				<script>
-					function makeAjaxRequest(opts){
+					function makeAjaxRequest(price){
 						$.ajax({
 						  type: "POST",
 						  data: { price: price },
