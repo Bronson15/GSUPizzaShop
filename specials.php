7b2ec1a362590f015b3c7c16834b64a8c060909c
Bronson Gregory Lane <bl02560@ad.georgiasouthern.edu>
2017-11-06 15:42:31 -0500

    testing

--- specials.php
+++ specials.php
@@ -5,39 +5,28 @@
 	<p>***CARRY OUT ONLY***</p>
 	
 	<div id="specials-1">
-		<form action="cart.php" >
+		<form action="cart.php" id="specials-form">
 			<fieldset>
 				<img src="/img/pizzapizza.jpg" alt="Pizza">
 				<br>
 				<button type="add" onclick="cart.php">Large Combo Pizza $8.00</button>	
 			</fieldset>
-		</form>
-		<form action="cart.php" >	
 			<fieldset>	
 				<img src= "/img/cheese.jpg" alt="CPizza">
 				<br>
 				<button type="add" onclick="cart.php" >Two Large Cheese Pizzas for $9.99</button>	
 			</fieldset>
-		</form>
 	</div>	
-	
-	<form action="cart.php" >
 		<fieldset>
 			<img src= "/img/meat.jpg" alt="MPizza">
 			<br>
 			<button type="add" onclick="cart.php" >Large Meat Lovers Pizza for $12.99</button>
 		</fieldset>
-	</form>
-	
-	<form action="cart.php" >
 		<fieldset>
 			<img src= "/img/3Tpizza.jpg" alt="3TPizza">
 			<br>
 			<button type="add" onclick="cart.php" >Large 3 topping Pizza for $7.00</button>
 		</fieldset>
-	</form>
-	
-	<form action="cart.php" >
 		<fieldset>
 			<img src= "/img/Mpizza.jpg" alt="MedPizza">
 			<button type="add" onclick="cart.php" >2 Medium 1 topping Pizza for $9.99</button>
