4a74251e460bebd7e388a33e03b87323af801f02
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-08 13:53:38 -0500

    initial-commit

---
+++ login.php
@@ -0,0 +1,10 @@
+<div id="log-col">
+	<form id="login" action="">
+		<fieldset id="username">
+			<label>USERNAME:</label>
+			<input type="text" id="user" value="username">
+			<label>PASSWORD:</label>
+			<input type="password" id="pass" value="password">
+		</fieldset>
+	</form>
+</div>
