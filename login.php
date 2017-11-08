72c005435d976b1f438292410ed2f2c100998b2a
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-08 13:57:01 -0500

    testing

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 	<form id="login" action="">
 		<fieldset id="username">
 			<label>USERNAME:</label>
-			<input type="text" id="user" value="username">
+			<input type="text" id="user" placeholder="username">
 			<label>PASSWORD:</label>
-			<input type="password" id="pass" value="password">
+			<input type="password" id="pass" placeholder="password">
 		</fieldset>
 	</form>
 </div>
