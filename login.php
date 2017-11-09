77086c43b62156400833bc83ca95e00ff2371324
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-08 20:06:58 -0500

    testing

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
-<?php include("header.php")
- ?>
-
+<?php include("header.php")?>
 <div id="log-col">
-	<form id="login" action="">
+	<form id="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 		<fieldset id="username">
 			
 			<label>USERNAME:</label>
@@ -12,11 +10,46 @@
 			<label>PASSWORD:</label>
 			<input type="password" id="pass" name="pass" placeholder="password">
 			<br>
+			<br>
 			<button type="submit" id="sub" name="log">LOGIN</button>
 			
 		</fieldset>
+		<?php 
+				$error = "";
+				include("database.php");
+				
+				$userParam = pg_query($pg_conn, "SELECT * FROM Customer WHERE username = $username;");
+				$passParam = pg_query($pg_conn, "SELECT * FROM Customer WHERE passw = $password;");
+				
+				$username_err = "";
+				$password_err = "";
+				
+				if($_SERVER["REQUEST_METHOD"] == "GET"){
+					$username = "";
+					$password = "";
+				}
+				else if($_SERVER["REQUEST_METHOD"] == "POST"){
+					$username = trim($_POST["user"]);
+					$password = trim($_POST["pass"]);
+					
+					if($userParam == $username && $passParam == $password ){
+						
+						setcookie("userIDforDV", $username, time()+43200);
+					}
+					else{
+						$error = "Your username and/or password is incorrect";
+					}
+				}
+				
+				$username = $_COOKIE['userIDforDV'];
+				if(isset($username) && $username !=""){
+					echo "Welcome" . $username;
+				}
+				
+				echo $error;
+				
+			?>
 	</form>
-	<!--https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php-->
 </div>
 <?php include("footer.php") ?>
 
