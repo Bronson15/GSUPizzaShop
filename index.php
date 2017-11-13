d1fbaeb5601ab408a142eee639a54ff6c267fb2a
Rosalie Fragnito <rf02095@ad.georgiasouthern.edu>
2017-11-13 15:39:19 -0500

    HomePage
    changing the homepage

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	
 	<div id="index-body-text">		
 		<main>
-		<h1>Welcome to GSU Pizza Shop!</h1>.
+		<h1>Welcome to GSU Pizza Shop!</h1>
 			h1{ 
 				font-family:verdana;
 				font-size: 40px; 
@@ -11,10 +11,8 @@
 		</main>
 		<!-- <img style="border-radius:20px" src="img/pizza.jpg"> -->
 		<p style="font-size: 160%"><i>
-			<a href= specials.php>
 				<img src="PizzaGus1.png" alt="Gus" style="float:left;width:432px;height:504px;">Check out the best deals
 				in town! 
-			</a>
 		</i></p>
 		
 	</div>
