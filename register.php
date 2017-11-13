939ad664e26e330ab5d3035a6a2c106b2c92b604
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-13 12:03:49 -0500

    creating-registration-form

---
+++ register.php
@@ -0,0 +1,39 @@
+<?php include("header.php")?>
+<div id="log-col">
+	<form id="login" action="/register.php" method="POST">
+		<fieldset id="username">
+			
+			<label>Name(First and Last):</label>
+			<input type="text" id="flname" name="flname" placeholder="EX. John Doe">
+			<br>
+			<br>
+			<label>Email Address:</label>
+			<input type="email" id="email" name="email" placeholder="jdoe@gsu.edu">
+			<br>
+			<br>
+			<label>Street Address:</label>
+			<input type="text" id="address" name="address" placeholder="House#, Street, City, State, Zip">
+			<br>
+			<br>
+			<label>Age(Must be at least 14):</label>
+			<input type="number" id="age" name="age" placeholder="Age" min="14">
+			<br>
+			<br>
+			<label>Contact Number</label>
+			<input type="tel" id="telephone" name="telephone" placeholder="xxx-xxx-xxxx">
+			<br>
+			<br>
+			<label>Username(max 15 characters):</label>
+			<input type="text" id="username" name="username" placeholder="EX. j.doe">
+			<br>
+			<br>
+			<label>Password(max 15 characters):</label>
+			<input type="password" id="password" name="password">
+			<br>
+			<br>
+			<button type="submit" id="sub" name="createUser">Create User</button>
+			
+		</fieldset>
+	</form>
+</div>
+<?php include("footer.php") ?>
