42486e359948daf0bde1097666c160bcf7161c17
Rosalie Fragnito <rf02095@ad.georgiasouthern.edu>
2017-11-13 15:43:27 -0500

    Homepage Test2
    fixing homepage

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php include("header.php"); ?>
-	
-	<div id="index-body-text">		
-		<main>
-		<h1>Welcome to GSU Pizza Shop!</h1>
-			h1{ 
+	h1{ 
 				font-family:verdana;
 				font-size: 40px; 
 				text-align: center; 
 			}
+	<div id="index-body-text">		
+		<main>
+		<h1>Welcome to GSU Pizza Shop!</h1>
+			
 		</main>
 		<!-- <img style="border-radius:20px" src="img/pizza.jpg"> -->
 		<p style="font-size: 160%"><i>
-				<img src="PizzaGus1.png" alt="Gus" style="float:left;width:432px;height:504px;">Check out the best deals
+				<img src="img/PizzaGus1.png" alt="Gus" style="float:left;width:432px;height:504px;">Check out the best deals
 				in town! 
 		</i></p>
 		
