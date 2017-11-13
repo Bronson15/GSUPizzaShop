42048040f913f0fc6ed75f5585411af09feb694b
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-13 15:44:23 -0500

    testing

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php include("header.php")
-	$query = "INSERT INTO Customer VALUES ('$_POST[name]', '$_POST[age]', '$_POST[telephone]','$_POST[email]', '$_POST[address]','$_POST[username]', '$_POST[password]')";
+	$query = "INSERT INTO customer (name,age,contactnumber, emailaddress, streetaddress,username, passw) VALUES ('$_POST[name]', '$_POST[age]', '$_POST[telephone]','$_POST[email]', '$_POST[address]','$_POST[username]', '$_POST[password]')";
 	
 	$result = pg_query($query);
 	
