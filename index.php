407804bf86eb305b4f21e32d761ea463923eb99c
Rosalie Fragnito <rf02095@ad.georgiasouthern.edu>
2017-11-13 15:32:03 -0500

    Homepage Changes
    try to make the homepage look nicer

--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 	
 	<div id="index-body-text">		
 		<main>
-			<p>Welcome to GSU pizza shop<p>
-			<p>Check out our best deals under specials</p>
-			test
+		<h1>Welcome to GSU Pizza Shop!</h1>.
+			h1{ 
+				font-family:verdana;
+				font-size: 40px; 
+				text-align: center; 
+			}
 		</main>
-		<img style="border-radius:20px" src="img/pizza.jpg">
+		<!-- <img style="border-radius:20px" src="img/pizza.jpg"> -->
+		<p style="font-size: 160%"><i>
+			<a href= specials.php>
+				<img src="PizzaGus1.png" alt="Gus" style="float:left;width:432px;height:504px;">Check out the best deals
+				in town! 
+			</a>
+		</i></p>
+		
 	</div>
 
 <?php include("footer.php");?>
