d294dceeeec6e8e295a732cf96ff49f9eb0e34c1
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-13 10:53:08 -0500

    testing

--- login.php
+++ login.php
@@ -1,26 +1,4 @@
-<?php include("header.php")
-	session_start();
-	if(isset($_POST['user']) && isset($_POST['pass'])){
-		$username = $_POST['user'];
-		$password = $_POST['pass'];
-		
-		$result = pg_query($pg_conn, "SELECT * FROM Customer WHERE username = '$username' AND passw = '$password'");
-		
-		if(pg_num_rows($result) > 0){
-			$_SESSION['valid_user'] = $username;
-		}
-	}
-	
-	pg_close($pg_conn);
-
-	if(isset($username)){
-		echo 'Could not log in';
-	}
-	else{
-		echo 'You' . $_SESSION['valid_user'] . ' are now logged in.';
-	}
-	
-?>
+<?php include("header.php")?>
 <div id="log-col">
 	<form id="login" action="login.php" method="POST">
 		<fieldset id="username">
