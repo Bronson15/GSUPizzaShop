e6b66c50f917382f4f89cacc0ba3d0fe5e6455b5
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-13 21:27:35 -0500

    testing-duplicates

--- register.php
+++ register.php
@@ -19,7 +19,7 @@
 			window.history.back();</script>';
 		}
 		//checks for duplicate users
-		else if($username == $userParam || ($name == $nameParam && $email == $emailParam)){
+		else if($_POST['username'] == $userParam || ($_POST['flname'] == $nameParam && $_POST['email'] == $emailParam)){
 			echo '<script type="text/javascript">alert("There is already an account with those credentials");
 			window.history.back();</script>';
 			
