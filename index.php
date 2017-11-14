f29de12f8d7dd56c04391bb8ab305f8e55a40684
dmenear <davidlmenear@gmail.com>
2017-11-14 09:41:47 -0500

    Update homepage

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
 			test
 		</main>
 		<img style="border-radius:20px" src="img/pizza.jpg">
+		<div id="pizza-deals">
+			<table>
+				<caption><b>Deals<b></caption>
+				<tbody>
+					<tr>
+					<td>
+					<!--image placed here-->
+						<b>Broke College Kid Special<b>
+						<p>Enjoy two large cheese pizzas for $9.99</p>
+					</td>
+					
+					<td>
+						<!--image placed here-->
+						<b>Mighty Meat Special<b>
+						<p>Grab a large meat lovers pizza for $12.99</p>
+					</td>
+					
+					<td>
+						<!--image placed here-->
+						<b>Toppings Galore<b>
+						<p>Customize your pizza! Large three topping pizza for $7.00</p>
+					</td>
+					</tr>
+			</table>
+		</div>
 	</div>
 
 <?php include("footer.php");?>
