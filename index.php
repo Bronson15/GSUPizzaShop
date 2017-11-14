5e90d4613c7fe1cb0d14b46c6878716897d7cc5d
Rosalie Fragnito <rf02095@ad.georgiasouthern.edu>
2017-11-14 08:17:12 -0500

    homepage

--- index.php
+++ index.php
@@ -8,17 +8,29 @@
 				font-size: 40px; 
 				text-align: center; 
 			}
+			
+		p{
+			text-align: center; 
+			font-family:verdana; 
+			font-size: 20px;
+		}
+		
+		.Gus{
+			text-align:center;
+			display: block; 
+			
+		}
+		
 		</style>
 		<h1>Welcome to GSU Pizza Shop!</h1>
 			
 		</main>
 		<!-- <img style="border-radius:20px" src="img/pizza.jpg"> -->
-		<p style="float:center;font-size: 160%"><i>
-				Check out the best deals
-				in town! 
+		<p><i>
+			Check out the best deals in town! 
 		</i></p>
 		<div id="pizza-gus">
-		<img src="img/PizzaGus1.png" alt="Gus" style="float:center;width:216px;height:252px;">
+		<img src="img/PizzaGus1.png" class="Gus" alt="Gus" style="float:center;width:216px;height:252px;">
 		</div>
 		
 	</div>
