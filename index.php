73ea291ab00bf3a7103787bedd682122a7f5307b
rosefragnito <rf02095@georgiasouthern.edu>
2017-11-15 11:48:49 -0500

    homepage

--- index.php
+++ index.php
@@ -4,15 +4,6 @@
 		<main>
 			<h2>Welcome to GSU Pizza Shop!</h2>
 		</main>
-		<style>
-			table,tr{
-			border: 0px;	
-			}	
-			
-			table{
-				border-spacing: 5px;	
-			}	
-		</style>
 		<br><img style="width:auto;" src="img/home_graphic.png"><br><br>
 		<div id="pizza-deals">
 			<table align="center">
