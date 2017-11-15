d1b707be8181fd3b62279bf4f0724095d809fa9f
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-15 15:53:45 -0500

    member

--- member.php
+++ member.php
@@ -1,7 +1,8 @@
 <?php include(header.php);
-	if(isset($_SESSION['username'])){
-		echo "<h1>" . $_SESSION['name'] . "</h1>";
+	
+	if(!$user->is_logged_in()){
+		echo "Welcome" . $_SESSION['name'];
 	}
+	
+	include(footer.php);
 ?>
-
-<?php include(footer.php);?>
