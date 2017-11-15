a893389e5e32ccf92e3179b7bfc58f99662f5c1b
Bronson Gregory Lane <bl02560@ad.georgiasouthern.edu>
2017-11-15 15:31:38 -0500

    buttons

--- cart.php
+++ cart.php
@@ -24,15 +24,22 @@
 				}
 			?>
 	</table>
+	<div id="cart-buttons">
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-		<button type='submit' id="clearCart" name='clear'>Clear Cart</button>
+			<button type='submit' id="clearCart" name='clearCart'>Clear Cart</button>
+			<button type='submit' id="checkout" name='checkout'>Checkout</button>
 		</form>
 		
-	<?php
-			if(isset($_POST['clear'])){
-				unset($_SESSION['cart']);
-			}	
-	?>	
+		<?php
+				if(isset($_POST['clearCart'])){
+					unset($_SESSION['cart']);
+				}	
+				
+				if(isset($_POST['checkout'])) {
+					header('Refresh: 1; URL = pizzatracker.php');
+				}
+		?>	
+	</div>	
 </main>
 	
 <?php include("footer.php");?>
