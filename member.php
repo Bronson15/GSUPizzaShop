26c2a5f4bbb5501d05482fbe80efac14915f1cb6
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-15 15:47:04 -0500

    member

--- member.php
+++ member.php
@@ -1,8 +1,5 @@
 <?php include(header.php);
-		echo "<h1>" . $_SESSION['name'] . "</h1><br><p>Name: '" . $_SESSION['name'] . "'<br>
-		<p>Address: '" . $_SESSION['address'] . "'<br>
-		<p>Contact Number: '" . $_SESSION['number'] . "'<br>
-		<p>Email: '" . $_SESSION['email'] . "'</p>";
+		echo "<h1>" . $_SESSION['name'] . "</h1>";
 ?>
 
 <?php include(footer.php);?>
