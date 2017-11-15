4269d39d875d7e88166adb32413a06cce3321b3b
dmenear <davidlmenear@gmail.com>
2017-11-15 12:41:28 -0500

    add basic cart

--- cart.php
+++ cart.php
@@ -1,7 +1,11 @@
 <?php include("header.php");?>
 	
 <main>
-		
+		<?php
+			foreach($_SESSION['cart'] as $item){
+				echo $item->toString()."<br>";
+			}
+		?>
 </main>
 	
 <?php include("footer.php");?>
