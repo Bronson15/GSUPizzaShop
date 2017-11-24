cec59d7058e8900ff014927a20f8190c6a25379c
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-24 11:49:07 -0500

    register-page

--- register.php
+++ register.php
@@ -48,26 +48,32 @@
 			<br>
 			<br>
 			<label>Email Address:</label>
+			<br>
 			<input type="email" id="email" name="email" placeholder="jdoe@gsu.edu">
 			<br>
 			<br>
 			<label>Street Address:</label>
+			<br>
 			<input type="text" id="address" name="address" placeholder="House#, Street, City, State, Zip">
 			<br>
 			<br>
 			<label>Age(Must be at least 14):</label>
+			<br>
 			<input type="number" id="age" name="age" placeholder="Age" min="14">
 			<br>
 			<br>
 			<label>Contact Number:</label>
+			<br>
 			<input type="text" id="telephone" name="telephone" placeholder="xxx-xxx-xxxx">
 			<br>
 			<br>
 			<label>Username(max 15 characters):</label>
+			<br>
 			<input type="text" id="username" name="username" placeholder="EX. j.doe" max="15">
 			<br>
 			<br>
 			<label>Password(max 15 characters):</label>
+			<br>
 			<input type="password" id="password" name="password" max="15">
 			<br>
 			<br>
