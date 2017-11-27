04ff7a3647b4810f012d94c239222c9ae0b2410e
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-26 22:18:53 -0500

    m

--- member.php
+++ member.php
@@ -1,6 +1,7 @@
 <?php include("header.php");
 
 	if(isset($_SESSION['username'])){
-		echo "Name: " . $_SESSION['name'];
+		echo "<p>Name: " . $_SESSION['name'] . "</p>";
 	}
+	else echo "<p>Must be logged in to view personal info</p>"
  include("footer.php");?>
