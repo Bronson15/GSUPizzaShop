9dfcd48096e57aa7c62a03167b4ffc063238f302
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-27 19:11:47 -0500

    pizzamap

--- specials.php
+++ specials.php
@@ -4,37 +4,39 @@
 	<h1>The Best Deals in Town: ALL DAY, EVERYDAY</h1>
 	<p>CARRY OUT ONLY</p>
 </main>
-	
+	<map name="pizzamap">
+		<area shape="rect" coords="2,470,5,470" alt="Pizza Special">
+	</map>
 	<div id="deals">
 	<table align="center">
 	<tbody>
 		<tr> 
 			<td> 
-			<img src="img/college_kid.png">
+			<img src="img/college_kid.png" usemap="#pizzamap">
 			</td>
 			
 			<td> 
-			<img src="img/combo_special.png">
+			<img src="img/combo_special.png" usemap="#pizzamap">
 			</td>
 		</tr>
 
 		<tr> 
 			<td> 
-			<img src="img/customize.png">
+			<img src="img/customize.png" usemap="#pizzamap">
 			</td>
 			
 			<td> 
-			<img src="img/double1.png">
+			<img src="img/double1.png" usemap="#pizzamap">
 			</td>
 		</tr>
 			
 		<tr> 
 			<td> 
-			<img src="img/meatlovers.png">
+			<img src="img/meatlovers.png" usemap="#pizzamap">
 			</td>
 			
 			<td> 
-			<img src="img/partypeeps.png">
+			<img src="img/partypeeps.png" usemap="#pizzamap">
 			</td>
 		</tr>	
 	</tbody>
