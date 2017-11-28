05fc11c8e83533191c43102430f584ad8a3020a2
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-27 20:07:23 -0500

    pizzamap

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 			</table>
 		</div>
 		<map name="ordernow">
-			<area shape="rect" coords="10,103,103,20" href="/cart.php" alt="Pizza Special">
+			<area shape="rect" coords="10,207,207,20" href="/cart.php" alt="Pizza Special">
 		</map>
 		</main>
 	</div>
