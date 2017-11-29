e77493e35b091ad1b6de7c8b2f6594a8dc99bc88
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2017-11-29 10:41:55 -0500

    html

--- register.js
+++ register.js
@@ -1,3 +1,14 @@
 function saveData(){
+	if(window.XMLHttpRequest){
+		xml = new XMLHttpRequest();
+	}
+	else{
+		xml = new ActiveXObject("Microsoft.XMLHTTP");
+	}
 	
+	xml.onreadystatechange = function(){
+		if(xml.readyState == 4 && xml.status == 200){
+			document.getElementById("")
+		}
+	}
 }
