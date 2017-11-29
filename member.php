4fd8087a6b9bbcc2c0ab6cc0f88877e348baf820
Bronson15 <bronsonlane@MacBook-2.local>
2017-11-29 11:26:49 -0500

    test

--- member.php
+++ member.php
@@ -8,40 +8,42 @@
 		<p> Address: <?php echo $_SESSION['address'] ?> </p>
 		<p> Email: <?php echo $_SESSION['email'] ?> </p>
 		<p> Phone: <?php echo $_SESSION['phone'] ?> </p>
-<?php
-	echo "Order History: ";
-	$query = "SELECT * FROM orders WHERE customerid = '" .$_SESSION['customerid']."'";
-	$result = pg_query($query);
-	$i = 0;
-	echo '<table><tr>';
-	while ($i < pg_num_fields($result)){
-		$fieldName = pg_field_name($result, $i);
-		echo '<td>' . $fieldName . '</td>';
-		$i = $i + 1;
-	}
-	echo '</tr>';
-	$i = 0;
-
-	while ($row = pg_fetch_row($result)) {
-		echo '<tr>';
-		$count = count($row);
-		$y = 0;
-		while ($y < $count){
-			$c_row = current($row);
-			echo '<td>' . $c_row . '</td>';
-			next($row);
-			$y = $y + 1;
+<div id="order-history">
+	<?php
+		echo "Order History: ";
+		$query = "SELECT * FROM orders WHERE customerid = '" .$_SESSION['customerid']."'";
+		$result = pg_query($query);
+		$i = 0;
+		echo '<table><tr>';
+		while ($i < pg_num_fields($result)){
+			$fieldName = pg_field_name($result, $i);
+			echo '<td>' . $fieldName . '</td>';
+			$i = $i + 1;
 		}
 		echo '</tr>';
-		$i = $i + 1;
-	}
-	pg_free_result($result);
+		$i = 0;
 
-	echo '</table>';
-	
-	}
-	else {
-?>
+		while ($row = pg_fetch_row($result)) {
+			echo '<tr>';
+			$count = count($row);
+			$y = 0;
+			while ($y < $count){
+				$c_row = current($row);
+				echo '<td>' . $c_row . '</td>';
+				next($row);
+				$y = $y + 1;
+			}
+			echo '</tr>';
+			$i = $i + 1;
+		}
+		pg_free_result($result);
+
+		echo '</table>';
+
+		}
+		else {
+	?>
+</div>
 	<p>	Must be logged in to view personal info </p>
 	</div>
 	<?php }
