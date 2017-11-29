621cc06d4f80b3be3cbc5bc69ae7daff00e473e0
Bronson15 <bronsonlane@MacBook-2.local>
2017-11-29 11:21:32 -0500

    TEST

--- member.php
+++ member.php
@@ -12,8 +12,33 @@
 	echo "Order History: ";
 	$query = "SELECT * FROM orders WHERE customerid = '" .$_SESSION['customerid']."'";
 	$result = pg_query($query);
-	$myarray = pg_fetch_all($result);
- 	print_r($myarray);
+	$i = 0;
+	echo '<table><tr>';
+	while ($i < pg_num_fields($result)){
+		$fieldName = pg_field_name($result, $i);
+		echo '<td>' . $fieldName . '</td>';
+		$i = $i + 1;
+	}
+	echo '</tr>';
+	$i = 0;
+
+	while ($row = pg_fetch_row($result)) {
+		echo '<tr>';
+		$count = count($row);
+		$y = 0;
+		while ($y < $count){
+			$c_row = current($row);
+			echo '<td>' . $c_row . '</td>';
+			next($row);
+			$y = $y + 1;
+		}
+		echo '</tr>';
+		$i = $i + 1;
+	}
+	pg_free_result($result);
+
+	echo '</table>';
+	
 	}
 	else {
 ?>
